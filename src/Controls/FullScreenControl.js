72fa1a2ee4ff6678d8c72666cbfbdbbd165aedbb
Joseph Kariuki <joehene@gmail.com>
2022-09-04 11:53:45 +0300

    Update FullScreenControl.js
    Components imported but not used. 
    ```
    ./src/Controls/FullScreenControl.js
      Line 1:8:   'React' is defined but never used     no-unused-vars
      Line 1:40:  'useState' is defined but never used  no-unused-vars
    ```

--- src/Controls/FullScreenControl.js
+++ src/Controls/FullScreenControl.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { FullScreen } from "ol/control";
 import MapContext from "../Map/MapContext";
 
